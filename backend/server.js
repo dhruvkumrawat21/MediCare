34314570fc35fdc2d69fdc98c70e2fbf8832568d
dhruv kumrawat <dhruvkumlap21@gmail.com>
2026-08-15 01:47:02 +0530

    added urls

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,8 @@ const app = express();
 const PORT = 4000;
 
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5176",
-    "http://localhost:5175"
+    "https://medicare-frontend-yi0l.onrender.com",
+    "https://medicare-admin-4ccd.onrender.com",
 ]
 
 
